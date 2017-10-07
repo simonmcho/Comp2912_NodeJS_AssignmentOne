706e0f17bb75f64c8f26a2d668fbdb7615a3e828
simonmcho <simonmcho@gmail.com>
2017-10-07 10:01:51 -0700

    capitalize first letters and clean up client js code

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,13 @@
 $(function(){
-    var $quantityDropdown = $(".pizza-quantity");
-
-    for(let i = 1; i <= 12; i++){
-        $quantityDropdown.append($('<option></option>').val(i).html(i));
+    
+    //Create dropdown for quantity
+    if($("select").hasClass(".pizza-quantity")) {
+        var $quantityDropdown = $(".pizza-quantity");
+        
+        for(let i = 1; i <= 12; i++){
+            $quantityDropdown.append($('<option></option>').val(i).html(i));
+        }
     }
+
+    
 });
